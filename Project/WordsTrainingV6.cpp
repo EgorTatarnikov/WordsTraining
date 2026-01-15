99cd4353a6db969802a44e97d2665a25b0686057
Egor Tatarnikov <egor13121994x@gmail.com>
2026-01-15 03:18:33 +0300

    Удалены лишние файлы

--- Project/WordsTrainingV6.cpp
+++ Project/WordsTrainingV6.cpp
@@ -3,9 +3,8 @@
 #include <vcl.h>
 #pragma hdrstop
 //---------------------------------------------------------------------------
-USEFORM("Unit3.cpp", Form3);
-USEFORM("LB7.cpp", Form2);
 USEFORM("Unit1.cpp", Form1);
+USEFORM("Unit3.cpp", Form3);
 //---------------------------------------------------------------------------
 WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 {
@@ -14,7 +13,6 @@ WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 		Application->Initialize();
 		SetApplicationMainFormOnTaskBar(Application, true);
 		Application->CreateForm(__classid(TForm3), &Form3);
-		Application->CreateForm(__classid(TForm2), &Form2);
 		Application->CreateForm(__classid(TForm1), &Form1);
 		Application->Run();
 	}
